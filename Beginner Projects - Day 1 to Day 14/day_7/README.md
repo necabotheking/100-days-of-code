545c66a7abca7a8a745cab6cea79cdf977421bfe
Aïcha Camara <aichaccamara@gmail.com>
2022-12-17 15:54:09 -0600

    Worked on day 7

---
+++ Beginner Projects - Day 1 to Day 14/day_7/README.md
@@ -0,0 +1,20 @@
+## 100 Days of Python Projects - Day 7
+[Python Pro Bootcamp](https://uc-cs.udemy.com/course/100-days-of-code/learn/lecture/20499928#overview "Udemy Course")
+
+### Project 7 - Hangman
+***
+## Instructions
+
+
+## Example Input
+
+```python
+Welcome to the tip calculator!
+What was the total bill? $124.56
+How much tip would you like to give? 10, 12, or 15? 12
+How many people to split the bill? 7
+```
+## Example Output
+```python
+Each person should pay: $19.93
+```
---
+++ Beginner Projects - Day 1 to Day 14/day_7/hangman.py
@@ -0,0 +1,42 @@
+# ---100 Days of Code---
+# -- Day 7 Project --
+
+import random
+
+from hangman_words import *
+from hangman_art import *
+
+print(logo)
+print("Welcome to Hangman!\n")
+
+chosen_word = random.choice(word_list)
+print(f"The word is {chosen_word}")
+
+# create the blank game board
+game_display = len(chosen_word) * ["_"]
+
+game_lives = 6
+game_end = False
+while not game_end:
+
+    guess = str(input("Please guess a letter:\n")).lower()
+
+    # check guessed letter
+    for letter in chosen_word:
+        if letter == guess:
+            game_display[chosen_word.index(guess)] = guess
+
+    if guess not in chosen_word:
+        print("\nOops Wrong Guess")
+        game_lives -= 1
+        if game_lives == 0:
+            game_end = True
+            print("You lose!")
+
+    print(f"{' '.join(game_display)}")
+
+    if "_" not in game_display:
+        game_end = True
+        print("You win!")
+
+    print(stages[game_lives])
