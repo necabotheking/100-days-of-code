40d51d0c04014f2b02273f55891eb3b0646aa4ab
Aïcha Camara <111548069+necabotheking@users.noreply.github.com>
2022-12-21 21:37:48 -0500

    started day 11

---
+++ Beginner Projects - Day 1 to Day 14/day_11/blackjack.py
@@ -0,0 +1,9 @@
+# ---100 Days of Code---
+# -- Day 11 Project --
+
+from art import *
+
+game = print("Do you want to play a game of blackjack? Type 'y' or 'n' \n:")
+
+if game = 'y':
+    print(logo)
