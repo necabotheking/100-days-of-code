5bdc2109d8cddde4f6b0afad69b71cb7af0468a8
Aïcha Camara <111548069+necabotheking@users.noreply.github.com>
2022-12-21 20:46:56 -0500

    Finish day 6

--- Beginner Projects - Day 1 to Day 14/day_6/maze.py
+++ Beginner Projects - Day 1 to Day 14/day_6/maze.py
@@ -1,2 +1,23 @@
 # ---100 Days of Code---
 # -- Day 6 Project --
+
+## Solutions to Reborg's World Challenge
+
+# def turn_right():
+#     turn_left()
+#     turn_left()
+#     turn_left()
+    
+# while front_is_clear():
+#     move()
+# turn_left()
+
+# while not at_goal():
+#     if right_is_clear():
+#         turn_right()
+#         move()
+#     elif front_is_clear():
+#         move()
+#     else:
+#         turn_left()
+    
