77d939993204ce84e3058bca6ea655eba8a20536
Aïcha Camara <111548069+necabotheking@users.noreply.github.com>
2022-12-21 20:43:31 -0500

    Update cipher.py

--- Beginner Projects - Day 1 to Day 14/day_8/cipher.py
+++ Beginner Projects - Day 1 to Day 14/day_8/cipher.py
@@ -1,7 +1,7 @@
 # ---100 Days of Code---
 # -- Day 8 Project --
 
-from cipher import *
+from cipher_art import *
 
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
