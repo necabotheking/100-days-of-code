d5848ad479015ee5bdb73690df64265035a86891
Aïcha Camara <111548069+necabotheking@users.noreply.github.com>
2022-12-29 18:26:02 -0500

    Started day 12

---
+++ Beginner Projects - Day 1 to Day 14/day_012/num_guess.py
@@ -0,0 +1,15 @@
+# ---100 Days of Code---
+# -- Day 12 Project --
+
+
+
+def guessing_game():
+    """
+    
+    """
+    print("Welcome to the Guessing Game!")
+    print("I'm thinking of a number between 1 and 100.")
+    difficulty = input("Choose a difficulty. Type 'easy' or 'hard':")
+    # if easy then 10 guesses
+    #if hard then 5 guesses
+guessing_game()
