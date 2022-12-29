1cd03287644c69e98465d51d1bbbcc1d85d9f6db
Aïcha Camara <111548069+necabotheking@users.noreply.github.com>
2022-12-29 18:02:13 -0500

    Finished day 11

--- Beginner Projects - Day 1 to Day 14/day_011/blackjack.py
+++ Beginner Projects - Day 1 to Day 14/day_011/blackjack.py
@@ -3,6 +3,7 @@
 
 from art import *
 import random
+import os
 
 ############### Our Blackjack House Rules #####################
 
@@ -16,42 +17,106 @@ import random
 ## Cards are not removed from the deck as they are drawn.
 ## The computer is the dealer.
 
-cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]  
-
-def deal_card(cards):
+def deal_card():
     """
-    Deals the user and the computer 2 cards each
+    Deals a card 
 
-    Inputs: (lst) list of cards
+    Inputs: 
+        None
 
-    Returns: (lst) lst of dealer and user cards
+    Returns: (int) card value
     """
-    user_cards = random.sample(cards, 2)
-    dealer_cards = random.sample(cards, 2)
-    return user_cards, dealer_cards
+    cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]  
+    card = random.choice(cards)
+    return card
 
 def calculate_score(player_hand):
     """
     Calculates the score 
     
-    Inputs: (lst) list of cards
+    Inputs: 
+        (lst) list of cards
 
     Returns: (int) score
     """
     if sum(player_hand) == 21 and len(player_hand) == 2:
         score = 0
-    else:
+    elif 11 in player_hand and sum(player_hand) > 21:
+        player_hand.remove(11)
+        player_hand.append(1)
         score = sum(player_hand)
-    # check if score is over 21 AND if there is an ace
-    # replace the ace with a 1
+    else: 
+        score = sum(player_hand)
     return score
 
-play_game = input("Do you want to play a game of blackjack? Type 'y' or 'n' \n:")
-game_end = True
+def compare_score(user_score, computer_score):
+    """
+    Compares the game scores of the user and the computer
 
-if play_game == 'y':
-    game_end = False
-    while not game_end:
-        print(logo)
+    Inputs: 
+        user_score (lst): list of cards
+        computer_score (lst): list of cards
 
+    Returns: (str) game outcome 
+    """
+    if user_score > 21 and computer_score > 21:
+        return "You went over. You lose"
+    elif user_score == computer_score:
+        return "Draw"
+    elif computer_score == 0:
+        return "Lose, opponent has Blackjack"
+    elif user_score == 0:
+        return "Win with a Blackjack"
+    elif user_score > 21:
+        return "You went over. You lose"
+    elif computer_score > 21:
+        return "Opponent went over. You win"
+    elif user_score > computer_score:
+        return "You win"
+    else:
+        return "You lose"
 
+def play_game():
+    """
+    Plays the blackjack game
+
+    Input:
+        None
+    
+    Returns: game outcome
+    """
+    print(logo)
+
+    user_cards = []
+    dealer_cards = []
+    game_over = False
+
+    for _ in range(2):
+        user_cards.append(deal_card())
+        dealer_cards.append(deal_card())  
+        
+    while not game_over:  
+        user_score = calculate_score(user_cards)
+        computer_score = calculate_score(dealer_cards)
+        print(f"   Your cards: {user_cards}, current score: {user_score}")
+        print(f"   Computer's first card: {dealer_cards[0]}")
+        if user_score == 0 or computer_score == 0 or user_score > 21:
+            game_over = True
+        else:
+            another_card = input("Type 'y' to draw another card, 'n' to pass: ")
+            if another_card == "y":
+                user_cards.append(deal_card())
+            else:
+                game_over = True
+
+    while computer_score != 0 and computer_score < 17:
+        dealer_cards.append(deal_card())
+        computer_score = calculate_score(dealer_cards) 
+
+    print(f"   Your final hand: {user_cards}, final score: {user_score}")
+    print(f"   Computer's final hand: {dealer_cards}, final score: {computer_score}")
+    print(compare_score(user_score, computer_score))
+
+while input("Do you want to play a game of Blackjack? Type 'y' or 'n': ") == "y":
+    os.system('clear')
+    play_game()
