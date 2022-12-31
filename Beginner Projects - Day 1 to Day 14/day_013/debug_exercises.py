37bd8b029e71d229748f4f3745ac1b6c05765233
Aïcha Camara <111548069+necabotheking@users.noreply.github.com>
2022-12-30 22:49:20 -0500

    started day 13

---
+++ Beginner Projects - Day 1 to Day 14/day_013/debug_exercises.py
@@ -0,0 +1,25 @@
+# ---100 Days of Code---
+# -- Day 13 Project --
+
+# Debug the code below: Odd or Even
+
+'''
+number = int(input("Which number do you want to check?"))
+
+if number % 2 = 0:
+    print("This is an even number.")
+else:
+    print("This is an off number.")
+'''
+
+number = int(input("Which number do you want to check?"))
+
+if number % 2 == 0:
+    print("This is an even number.")
+else:
+    print("This is an off number.")
+
+# Debug the code below: Leap Year
+
+
+# Debug the code below: FizzBuzz
