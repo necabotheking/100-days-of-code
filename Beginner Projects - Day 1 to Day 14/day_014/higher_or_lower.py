4a876cb53d3c7e5f9eec02fa3aebe3713a46c356
Aïcha Camara <111548069+necabotheking@users.noreply.github.com>
2022-12-31 15:04:52 -0500

    Update day 14

--- Beginner Projects - Day 1 to Day 14/day_014/higher_or_lower.py
+++ Beginner Projects - Day 1 to Day 14/day_014/higher_or_lower.py
@@ -3,3 +3,46 @@
 
 from art import *
 from game_data import *
+
+def check_win():
+    """
+    Check's if the player chose the right choice
+    
+    Inputs:
+        [Inputs Here]
+    
+    Returns:
+    """
+    pass
+ 
+def outcome():
+    """
+    Based on if the player won or lost the outcome is given
+    
+    Inputs:
+        [Inputs Here]
+
+    Returns:
+    """
+    pass
+
+
+def higher_lower():
+    """
+    Plays the Higher or Lower Game
+
+    Inputs: 
+        None
+
+    Returns: Outcome of the game
+    """
+    print(logo)
+    print(f"Compare A: {}")
+    print(vs)
+    print(f"Against B: {}")
+    choice = str(input("Who has more followers? Type 'A' or 'B': ")).lower()
+
+    pass
+
+
+higher_lower()
